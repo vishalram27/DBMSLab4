27ac6da9c88966a909b33a36c090f7ae4b332dbe
Ramachandran S <134491955+vishalram27@users.noreply.github.com>
2023-12-06 10:48:04 +0530

    Lab_4_Q4.sql

--- DBMSLab4/SQL/Lab_4_Q4.sql
+++ DBMSLab4/SQL/Lab_4_Q4.sql
@@ -1,15 +1,15 @@
 -- Display all the orders along with product name ordered by a customer having Customer_Id=2
 
-select q.cus_id, q.cus_name,q.ord_id, product.pro_name from
+SELECT q.cus_id, q.cus_name,q.ord_id, product.pro_name FROM
 (
-	select c.cus_id,c.cus_name, p.ord_id,p.ord_amount, p.pro_id from
+	SELECT c.cus_id,c.cus_name, p.ord_id,p.ord_amount, p.pro_id FROM
 	(
 		SELECT O.*, sp.pro_id FROM orders AS O
-		inner join supplier_pricing as sp
-		where o.pricing_id=sp.pricing_id
-	) as p
-	inner join customer as c
-	where p.cus_id=c.cus_id and c.cus_id=2
-) as q
-inner join product
-on q.pro_id= product.pro_id;
+		INNER Join supplier_pricing AS sp
+		WHERE o.pricing_id=sp.pricing_id
+	) AS p
+	INNER Join customer AS c
+	WHERE p.cus_id=c.cus_id AND c.cus_id=2
+) AS q
+INNER Join product
+ON q.pro_id= product.pro_id;
