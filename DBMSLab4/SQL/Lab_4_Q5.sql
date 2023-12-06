391673522f40dca41ac516919ffc7e6a28019de0
Ramachandran S <134491955+vishalram27@users.noreply.github.com>
2023-12-06 10:50:40 +0530

    Lab_4_Q5.sql

--- DBMSLab4/SQL/Lab_4_Q5.sql
+++ DBMSLab4/SQL/Lab_4_Q5.sql
@@ -1,8 +1,7 @@
 -- Display the Supplier details who can supply more than three product.
--- As sir asked to change it has more than three and the SQL has been written for
--- Display the Supplier details who can supply more than three product.
+-- As sir asked to change it has more than three and the SQL has been written for Display the Supplier details who can supply more than three product.
 
-select supp.* from supplier as supp where supp.supp_id in
+SELECT supp.* FROM supplier AS supp WHERE supp.supp_id IN
 (
-select sp.supp_id from supplier_pricing as sp group by sp.supp_id having count(sp.supp_id)>3
+SELECT sp.supp_id FROM supplier_pricing AS sp GROUP BY sp.supp_id HAVING COUNT(sp.supp_id)>3
 );
