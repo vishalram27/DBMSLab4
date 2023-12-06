0710ce1449babc2a80174a70dc74971328e5fc14
Ramachandran S <134491955+vishalram27@users.noreply.github.com>
2023-12-06 11:00:10 +0530

    Lab_4_Q6.sql

--- DBMSLab4/SQL/Lab_4_Q6.sql
+++ DBMSLab4/SQL/Lab_4_Q6.sql
@@ -1,11 +1,11 @@
 -- Find the least expensive product from each category and print the table with category id, name, and price of the product
 
-select category.cat_id,category.cat_name, min(t3.min_price) as Min_Price from category 
-inner join
+SELECT category.cat_id,category.cat_name, min(t3.min_price) AS Min_Price FROM category 
+INNER JOIN
 (
-	select product.cat_id, product.pro_name, t2.* from product 
-    inner join
-	(
-		select pro_id, min(supp_price) as Min_Price from supplier_pricing group by pro_id
-	) as t2 where t2.pro_id = product.pro_id
-) as t3 where t3.cat_id = category.cat_id group by t3.cat_id;
+	SELECT product.cat_id, product.pro_name, t2.* FROM product 
+INNER JOIN
+(		
+	SELECT pro_id, min(supp_price) AS Min_Price FROM supplier_pricing GROUP BY pro_id
+	) AS t2 WHERE t2.pro_id = product.pro_id
+) AS t3 WHERE t3.cat_id = category.cat_id GROUP BY t3.cat_id;
